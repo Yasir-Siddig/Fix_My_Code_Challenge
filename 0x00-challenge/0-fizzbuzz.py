93a590417be8384f608ce0090ab6af698562d39c
yasir324 <yasir.c.s.ltd.sa@gmail.com>
2023-11-18 09:45:58 +0300

    edit fizzbuzz 1

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -15,17 +15,17 @@ def fizzbuzz(n):
     if n < 1:
         return
 
-    tmp_result = []
-    for i in range(1, n + 1):
-        if (i % 3) == 0 and (i % 5) == 0:
-            tmp_result.append("FizzBuzz")
-        elif (i % 5) == 0:
-            tmp_result.append("Fizz")
-        elif (i % 3) == 0:
-            tmp_result.append("Buzz")
-        else:
-            tmp_result.append(str(i))
-    print(" ".join(tmp_result))
+  # - Fizzbuzz Exercise -
+
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print('fizzbuzz')
+    elif i % 3 == 0:
+        print('fizz')
+    elif i % 5 == 0:
+        print('buzz')
+    else:
+        print(i)
 
 
 if __name__ == '__main__':
